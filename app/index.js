8211e18d2a2c3c436684664e16d92f4d89163277
G. Hussain Chinoy <ghchinoy@gmail.com>
2015-06-29 19:41:09 -0600

    lint

--- app/index.js
+++ app/index.js
@@ -15,11 +15,11 @@ module.exports = generators.Base.extend({
     });
 
     this.option('flat', {
-	  type: Boolean,
-	  required: false,
-	  defaults: false,
-	  desc: 'When specified, custom starter will be created at the top level of the project.'
-	});
+      type: Boolean,
+      required: false,
+      defaults: false,
+      desc: 'When specified, custom starter will be created at the top level of the project.'
+    });
   },
 
   initializing: function () {
@@ -27,48 +27,49 @@ module.exports = generators.Base.extend({
   },
 
   prompting: {
-  	askFor: function () {
-	  	var done = this.async();
-	  	this.log(yosay('Hello! Let\'s create an Akana Community Manager Theme customization.'));
+    askFor: function () {
+      var done = this.async();
+      this.log(yosay('Hello! Let\'s create an Akana Community Manager Theme customization.'));
 
-	  	var prompts = [{
+      var prompts = [{
         type: 'input',
-	  		name: 'companyName',
-	  		message: 'Please provide a company name',
-	  		default: 'Custom Starter'
-	  	},
-      {
-        type: 'input',
-        name: 'author',
-        message: 'Author',
-        default: 'you'
+        name: 'companyName',
+        message: 'Please provide a company name',
+        default: 'Custom Starter'
       },
-      {
-        type: 'input',
-        name: 'cmSiteTitle',
-        message: 'Title for CM site',
-        default: 'API Catalog'
-      },
-      {
-        type: 'input',
-        name: 'mainColor',
-        message: 'Base color for theme',
-        default: '#2683b4'
-      },
-      {
-        type: 'input',
-        name: 'landingHeadline',
-        message: 'Landing page Headline',
-        default: 'Extend your Reach'
-      },
-      {
-        type: 'input',
-        name: 'landingTagline',
-        message: 'Landing page Tagline',
-        default: 'APIs you can trust'
-      }
-      ];
+        {
+          type: 'input',
+          name: 'author',
+          message: 'Author',
+          default: 'you'
+        },
+        {
+          type: 'input',
+          name: 'cmSiteTitle',
+          message: 'Title for CM site',
+          default: 'API Catalog'
+        },
+        {
+          type: 'input',
+          name: 'mainColor',
+          message: 'Base color for theme',
+          default: '#2683b4'
+        },
+        {
+          type: 'input',
+          name: 'landingHeadline',
+          message: 'Landing page Headline',
+          default: 'Extend your Reach'
+        },
+        {
+          type: 'input',
+          name: 'landingTagline',
+          message: 'Landing page Tagline',
+          default: 'APIs you can trust'
+        }
+        ];
 
+      // check for atmotool option / flag
       if (this.options.atmotool) {
         prompts.push(
           {
@@ -84,34 +85,34 @@ module.exports = generators.Base.extend({
             default: 'administrator@cm.akana.demo'
           }
         );
-       }
+      }
 
-	  	this.prompt(prompts, function (props) {
+      this.prompt(prompts, function (props) {
         this.props = props;
         this.config.set(props);
         this.companyName = props.companyName;
-	  		done();
-	  	}.bind(this));
-	  }
+        done();
+      }.bind(this));
+    }
 
   },
 
   writing: {
     projectfiles: function () {
-    	this.companyDir = 'CM_' + this.companyName.replace(/\s/g, '') + '/';
+      this.companyDir = 'CM_' + this.companyName.replace(/\s/g, '') + '/';
 
       this.template('README.md', this.companyDir + 'README.md');
       this.template('custom.less', this.companyDir +  'custom.less');
       this.directory('resources',  this.companyDir + 'resources');
       this.directory('content', this.companyDir + 'content');
       if (this.options.atmotool) {
-         this.template('local.conf', this.companyDir + 'local.conf');
+        this.template('local.conf', this.companyDir + 'local.conf');
       }
-  	 },
+    },
 
-     config: function() {
+    config: function () {
       this.directory('config');
-     }
+    }
   }
 
 });
